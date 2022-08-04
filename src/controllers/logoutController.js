d7a7586a8e4454e40198700e564835541c6faf70
Eduardo Hernandez <eehernandez@gmail.com>
2022-08-04 19:09:55 -0300

    Comentado console.log

--- src/controllers/logoutController.js
+++ src/controllers/logoutController.js
@@ -4,19 +4,15 @@ const { HTTP_STATUS_OK } = require("../../public/assets/scripts/const");
 
 const logout = async (req, res) => {
   const logoutPage =
-  fs.readFileSync(
-    path.join(__dirname, "../../public/assets/views/logout.hbs")
-  ) + "";
+    fs.readFileSync(path.join(__dirname, "../../public/assets/views/logout.hbs")) + "";
   try {
     if (req.session?.nombre) {
       const usuario = req.session?.nombre;
       const result = await req.session.destroy();
-      console.log(res);
+      //console.log(res);
       if (result.cookie) {
         res.baseUrl = "/";
-        res
-          .status(HTTP_STATUS_OK)
-          .send(logoutPage.replace("{{{body}}}", "Hasta luego " + usuario));
+        res.status(HTTP_STATUS_OK).send(logoutPage.replace("{{{body}}}", "Hasta luego " + usuario));
       } else {
         res.redirect("/");
       }
